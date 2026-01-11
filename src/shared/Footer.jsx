3083187ae3981b7ba52f93c62dbd579c52ce863d
MH-liton-360 <liton72524nk@gmail.com>
2026-01-11 23:46:52 +0600

    adding support and quick links

--- src/shared/Footer.jsx
+++ src/shared/Footer.jsx
@@ -17,19 +17,17 @@ const Footer = () => {
                     <nav>
                         <h6 className="footer-title">Services</h6>
                         <a className="link link-hover">Branding</a>
-                        <a className="link link-hover">Design</a>
                         <a className="link link-hover">Marketing</a>
                         <a className="link link-hover">Advertisement</a>
                     </nav>
                     <nav>
-                        <h6 className="footer-title">Company</h6>
+                        <h6 className="footer-title">Help & Support</h6>
                         <a className="link link-hover">About us</a>
                         <a className="link link-hover">Contact</a>
-                        <a className="link link-hover">Jobs</a>
-                        <a className="link link-hover">Press kit</a>
+                        <a className="link link-hover">Feedback</a>
                     </nav>
                     <nav>
-                        <h6 className="footer-title">Legal</h6>
+                        <h6 className="footer-title">Quick Links</h6>
                         <a className="link link-hover">Terms of use</a>
                         <a className="link link-hover">Privacy policy</a>
                         <a className="link link-hover">Cookie policy</a>
