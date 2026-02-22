d6761976d7b5644be9b5736b0fc5d550a439c33c
MH-liton-360 <liton72524nk@gmail.com>
2026-02-23 00:16:07 +0600

    footer navigation section change

--- src/shared/Footer.jsx
+++ src/shared/Footer.jsx
@@ -2,11 +2,11 @@ const Footer = () => {
     return (
         <section>
             <div>
-                <footer class="bg-amber-50 text-emerald-950 rounded-t-xl p-10">
+                <footer class="bg-amber-50 text-emerald-950 rounded-t-xl pt-10">
                     <div class="max-w-6xl mx-auto flex flex-col items-center gap-6">
 
                         {/* Navigation Links */}
-                        <nav class="flex flex-wrap justify-center gap-6 font-semibold">
+                        <nav class="flex flex-wrap justify-center gap-6">
                             <a class="link link-hover">About us</a>
                             <a class="link link-hover">Terms of Use</a>
                             <a class="link link-hover">Privacy Statement</a>
@@ -23,7 +23,7 @@ const Footer = () => {
                         <hr class="w-full border-emerald-950 opacity-30" />
 
                         {/* Copyright */}
-                        <div class="text-center text-xs text-emerald-950">
+                        <div class="text-center text-xs text-emerald-950 pb-3">
                             Copyright © {new Date().getFullYear()} - Global Matrimony Ltd. All Rights Reserved
                         </div>
 
