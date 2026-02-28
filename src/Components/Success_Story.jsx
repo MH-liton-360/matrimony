49f0365774da2a3bd487dfb3656b8911ad6a566e
MH-liton-360 <liton72524nk@gmail.com>
2026-02-28 18:50:50 +0600

    again story push

--- src/Components/Success_Story.jsx
+++ src/Components/Success_Story.jsx
@@ -1,20 +1,156 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-import { use } from 'react';
-import Story from './Story';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
 
-const Success_Story = ({ storyPromise }) => {
-    const success_Story = use(storyPromise);
-    console.log(success_Story);
 
+// import required modules
+import { FreeMode, Pagination } from 'swiper/modules';
+
+
+import slide01 from '../assets/bg-image/Warm embrace under golden lights.png'
+
+
+const Success_Story = () => {
     return (
+        <>
+            <Swiper
+                slidesPerView={3}
+                spaceBetween={30}
+                freeMode={true}
+                pagination={{
+                    clickable: true,
+                }}
+                modules={[FreeMode, Pagination]}
+                className="mySwiper"
+            >
+                {/* Slide Start Now Here!  */}
 
-        <div className="card">
-            <h3>success_Story:{success_Story.length} </h3>
-            {
-                success_Story.map(story => <Story story={story}></Story>)
-            }
-        </div>
+                {/* Swiper slide 01  */}
+                <SwiperSlide>
+                    <div className="card bg-base-100 w-96 shadow-sm">
+                        <figure className="px-10 pt-10">
+                            <img
+                                src={slide01}
+                                alt="Shoes"
+                                className="rounded-xl" />
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
 
+                        </div>
+                    </div>
+                </SwiperSlide>
+                {/* Swiper slide 01  */}
+                <SwiperSlide>
+                    <div className="card bg-base-100 w-96 shadow-sm">
+                        <figure className="px-10 pt-10">
+                            <img
+                                src={slide01}
+                                alt="Shoes"
+                                className="rounded-xl" />
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+
+                        </div>
+                    </div>
+                </SwiperSlide>
+                {/* Swiper slide 01  */}
+                <SwiperSlide>
+                    <div className="card bg-base-100 w-96 shadow-sm">
+                        <figure className="px-10 pt-10">
+                            <img
+                                src={slide01}
+                                alt="Shoes"
+                                className="rounded-xl" />
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+
+                        </div>
+                    </div>
+                </SwiperSlide>
+                {/* Swiper slide 01  */}
+                <SwiperSlide>
+                    <div className="card bg-base-100 w-96 shadow-sm">
+                        <figure className="px-10 pt-10">
+                            <img
+                                src={slide01}
+                                alt="Shoes"
+                                className="rounded-xl" />
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+
+                        </div>
+                    </div>
+                </SwiperSlide>
+                {/* Swiper slide 01  */}
+                <SwiperSlide>
+                    <div className="card bg-base-100 w-96 shadow-sm">
+                        <figure className="px-10 pt-10">
+                            <img
+                                src={slide01}
+                                alt="Shoes"
+                                className="rounded-xl" />
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+
+                        </div>
+                    </div>
+                </SwiperSlide>
+                {/* Swiper slide 01  */}
+                <SwiperSlide>
+                    <div className="card bg-base-100 w-96 shadow-sm">
+                        <figure className="px-10 pt-10">
+                            <img
+                                src={slide01}
+                                alt="Shoes"
+                                className="rounded-xl" />
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+
+                        </div>
+                    </div>
+                </SwiperSlide>
+                {/* Swiper slide 01  */}
+                <SwiperSlide>
+                    <div className="card bg-base-100 w-96 shadow-sm">
+                        <figure className="px-10 pt-10">
+                            <img
+                                src={slide01}
+                                alt="Shoes"
+                                className="rounded-xl" />
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+
+                        </div>
+                    </div>
+                </SwiperSlide>
+                {/* Swiper slide 01  */}
+                <SwiperSlide>
+                    <div className="card bg-base-100 w-96 shadow-sm">
+                        <figure className="px-10 pt-10">
+                            <img
+                                src={slide01}
+                                alt="Shoes"
+                                className="rounded-xl" />
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+
+                        </div>
+                    </div>
+                </SwiperSlide>
+            </Swiper>
+        </>
     );
 };
 
