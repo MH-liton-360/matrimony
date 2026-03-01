be7ad5ab4add17e3582f61cc47d09df73c5b8bbd
MH-liton-360 <liton72524nk@gmail.com>
2026-03-01 22:51:09 +0600

    Mobile view

--- src/Components/Success_Story.jsx
+++ src/Components/Success_Story.jsx
@@ -53,7 +53,7 @@ const Success_Story = () => {
                 modules={[FreeMode, Pagination]}
                 className="mySwiper py-10"
                 breakpoints={{
-                    320: { slidesPerView: 1, spaceBetween: 20 },
+                    320: { slidesPerView: 1, spaceBetween: 20 },   //Mobile view
                     640: { slidesPerView: 2, spaceBetween: 20 },
                     1024: { slidesPerView: 3, spaceBetween: 30 },
                 }}
