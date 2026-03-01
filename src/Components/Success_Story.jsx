e2dcb48beef8c46eb63892aadbb70c46b8d13eb1
MH-liton-360 <liton72524nk@gmail.com>
2026-03-01 22:55:23 +0600

    Today end commit

--- src/Components/Success_Story.jsx
+++ src/Components/Success_Story.jsx
@@ -54,7 +54,7 @@ const Success_Story = () => {
                 className="mySwiper py-10"
                 breakpoints={{
                     320: { slidesPerView: 1, spaceBetween: 20 },   //Mobile view
-                    640: { slidesPerView: 2, spaceBetween: 20 },  //Laptop view
+                    640: { slidesPerView: 2, spaceBetween: 20 },  //Tablet view
                     1024: { slidesPerView: 3, spaceBetween: 30 },//Desktop view
                 }}
             >
