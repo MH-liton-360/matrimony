fec2c642e1fa91933091fd8c7a0a010debb084de
MH-liton-360 <liton72524nk@gmail.com>
2026-03-02 23:24:21 +0600

    Helmet ins

--- src/main.jsx
+++ src/main.jsx
@@ -8,10 +8,14 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import router from './router/router.jsx';
+import { HelmetProvider } from 'react-helmet-async';
+
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <HelmetProvider>
+      <RouterProvider router={router} />
+    </HelmetProvider>
   </StrictMode>,
 )
