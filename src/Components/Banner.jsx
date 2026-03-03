653f974adb6ad8c078cdc135152a66606b07f6b5
MH-liton-360 <liton72524nk@gmail.com>
2026-03-03 23:51:06 +0600

    banner modify

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -3,6 +3,9 @@ import bannerImg from '../assets/Banner/BG-matrimony-image.png'
 const Banner = () => {
     return (
         <div className="">
+            <div className="">
+                <p>Find Your Perfect Match for a Halal & Happy Marriage</p>
+            </div>
             <img src={bannerImg} alt="" />
         </div>
     )
