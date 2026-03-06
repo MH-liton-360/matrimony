f8210c0ee2f7b3f4ad779c005b2cc91409386a25
MH-liton-360 <liton72524nk@gmail.com>
2026-03-06 23:44:06 +0600

    Recently_Join_Members_Card link add

--- src/Components/Recently_Join_Members/Recently_Join_Members.jsx
+++ src/Components/Recently_Join_Members/Recently_Join_Members.jsx
@@ -1,8 +1,10 @@
+import Recently_Join_Members_Card from "./Recently_Join_Members_Card";
 
 const Recently_Join_Members = () => {
     return (
         <div>
             <h3>Recently_Join_Members</h3>
+            <Recently_Join_Members_Card></Recently_Join_Members_Card>
         </div>
     );
 };
