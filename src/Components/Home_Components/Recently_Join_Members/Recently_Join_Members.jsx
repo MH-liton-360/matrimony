28a092854f576d2269370a02c30ce4ae5070d5c2
MH-liton-360 <liton72524nk@gmail.com>
2026-03-08 23:20:41 +0600

    Join members heading add

--- src/Components/Home_Components/Recently_Join_Members/Recently_Join_Members.jsx
+++ src/Components/Home_Components/Recently_Join_Members/Recently_Join_Members.jsx
@@ -1,11 +1,14 @@
-import Recently_Join_Members_Card from "./Recently_Join_Members_Card";
+import SectionTitle from "../SectionTitle/SectionTitle";
 
 const Recently_Join_Members = () => {
     return (
-        <div>
-            <h3>Recently_Join_Members</h3>
-            <Recently_Join_Members_Card></Recently_Join_Members_Card>
-        </div>
+        <section className="">
+            <SectionTitle
+                heading={"Recently Join Members"}
+            >
+
+            </SectionTitle>
+        </section>
     );
 };
 
--- src/Components/Home_Components/Recently_Join_Members/Recently_Join_Members_Card.jsx
+++ src/Components/Home_Components/Recently_Join_Members/Recently_Join_Members_Card.jsx
@@ -1,8 +1,8 @@
 
 const Recently_Join_Members_Card = () => {
     return (
-        <div>
-            <h3>Recently_Join_Members_Card</h3>
+        <div className="">
+
         </div>
     );
 };
