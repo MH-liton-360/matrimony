f017dd2abee45b4d6404c037a0160266069d4862
MH-liton-360 <liton72524nk@gmail.com>
2026-03-13 23:01:08 +0600

    Landing again setup

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,9 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
 import Login from "../pages/Login";
-import Landing from "../pages/Landing";
+import Landing from "../pages/Landing/Landing";
 import SignUp from "../pages/SignUp";
-import Home from "../pages/Home";
+import Home from "../pages/Home/Home";
 import Error from "../Components/Error/Errors/Error";
 
 export const router = createBrowserRouter([
