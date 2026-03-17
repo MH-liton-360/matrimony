7d3e32423e00b24f833a523467da9d85bda9b429
MH-liton-360 <liton72524nk@gmail.com>
2026-03-17 23:56:34 +0600

    import authcontext and provider

--- src/router/Secure/SecureRoute.jsx
+++ src/router/Secure/SecureRoute.jsx
@@ -1,5 +1,9 @@
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
 
 const SecureRoute = () => {
+
+    const { user } = useContext(AuthContext);
     return (
         <div>
 
