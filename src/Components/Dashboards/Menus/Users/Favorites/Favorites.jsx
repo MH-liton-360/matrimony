87af080a261893f0cbe683b141202d41d61d5c66
MH-liton-360 <liton72524nk@gmail.com>
2026-03-25 23:12:16 +0600

    modify favorites

---
+++ src/Components/Dashboards/Menus/Users/Favorites/Favorites.jsx
@@ -0,0 +1,14 @@
+const Favorites = () => {
+    return (
+        <section>
+
+            <title>Favorites - Matrimony</title>
+
+            <div>
+                <h1>This is favorites</h1>
+            </div>
+        </section>
+    );
+};
+
+export default Favorites;
