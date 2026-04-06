44240f6d7eebb86642ec6a0f25653c4acae2a3a7
MH-liton-360 <liton72524nk@gmail.com>
2026-04-06 15:09:00 +0600

    navbar route setup

--- src/layouts/Dashboard/Dashboard.jsx
+++ src/layouts/Dashboard/Dashboard.jsx
@@ -4,6 +4,7 @@ import adminMenu from "../../Components/Dashboards/Menu/Admin/AdminMenu";
 import { Outlet } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
+import DashboardNavbar from "../../shared/DashboardNavbar";
 
 const Dashboard = ({ role }) => {
     // const role = "admin";
@@ -17,10 +18,28 @@ const Dashboard = ({ role }) => {
             <div className="bg-gray-50 min-h-screen">
                 <Sidebar user={user} logOut={logOut} menus={menus} />
 
+                {/* navbar  */}
+                <div style={{ flex: 1, position: "relative" }}>
+                    {/* Navbar fixed with inline CSS */}
+                    <div style={{
+                        position: "fixed",
+                        top: 0,
+                        left: 288,  // ml-72 = 18rem = 288px
+                        width: "calc(100% - 288px)",
+                        backgroundColor: "#333",
+                        color: "#fff",
+                        padding: "12px 20px",
+                        zIndex: 1000
+                    }}>
+                        Dashboard Navbar
+                    </div>
+                </div>
+
                 {/* Outlet  */}
                 <div className="ml-72 min-h-screen p-6">
                     <Outlet />
                 </div>
+
             </div>
         </section>
     );
